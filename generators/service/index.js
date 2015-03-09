ad647ca5738afb97840af4abc49c9d09c22e6214
Loet Jansen van Rensburg <loet.jv.rensburg@gmail.com>
2015-03-09 21:54:14 +0200

    init

---
+++ generators/service/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var yeoman = require('yeoman-generator');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function() {
+    this.argument('name', {type: String, required: true});
+
+    this.composeWith('yangular:service', {
+      args: [this.name],
+      options: {
+        scriptsDir: this.config.get('scriptsDir')
+      }
+    });
+  }
+});
